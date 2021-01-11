49a1fa72f34606b274f991c3459d6e36061e7340
tiborizsak <tibor.izsak89@gmail.com>
2021-01-11 02:31:56 +0100

    Low light test // restored

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from fractions import Fraction
 from time import sleep
 
 with picamera.PiCamera() as camera:
-    camera.framerate = Fraction(1, 6)
-    camera.sensor_mode = 3
-    camera.shutter_speed = 6000000
-    camera.iso = 300
-    sleep(30)
-    camera.exposure_mode = 'off'
+    #This section is used in case of low light capture
+    # camera.framerate = Fraction(1, 6)
+    # camera.sensor_mode = 3
+    # camera.shutter_speed = 6000000
+    # camera.iso = 300
+    # sleep(30)
+    # camera.exposure_mode = 'off'
     camera.rotation = 180
     camera.resolution = (1920, 1080)
     camera.start_recording('my_video.h264')
